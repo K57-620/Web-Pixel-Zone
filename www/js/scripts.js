ab3209649637421c3e1fc8b5670f34169676aa92
K57-620 <adrian2008cado@gmail.com>
2026-02-20 10:25:04 +0100

    Add files via upload

---
+++ www/js/scripts.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+	new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,eu,gl,en,fr,it,pt,de,esp', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true}, 'google_translate_element');
+        }
